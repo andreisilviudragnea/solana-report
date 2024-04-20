bd4a9b4f0354ba70ae711494692359e398dc0a2f
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-04-20 15:52:22 +0300

    More txs

--- src/check_neon_tx_log_truncated_iterative.rs
+++ src/check_neon_tx_log_truncated_iterative.rs
@@ -29,7 +29,10 @@ async fn check_neon_tx_log_truncated_iterative() {
             // serialize(&"0xc410f39dd20263f9631aaa748ad0c6b03da88a96749689620800d3ddb96bf351"),
             // serialize(&"0x905cbe45bb54390f9a40a9607940bce626afa9e758ca74c6673eaef1a7b7e97f"),
             // serialize(&"0x294f306c846dcd086fb4cb63aa7012364f839f5d60a6e5db69ab70a33612f996"),
-            serialize(&"0x90ba5ccb669714ab8480c88c1620e8c85447e5cf2b47763a216f74505a27ad18"),
+            // serialize(&"0x90ba5ccb669714ab8480c88c1620e8c85447e5cf2b47763a216f74505a27ad18"),
+            // serialize(&"0x90914c6d70b48c3c98ff5b116a617784a506211384b145a4f70c50e1aefdb4a0"),
+            // serialize(&"0xb6a28891110884003067465bf8d5943669a5779264e2b0a118e937aae374be1d"),
+            serialize(&"0xb922b02f684e734937bd246d7a63a164f124e76333360363ae153b958e4b61de"),
             "solanaTransactionList".into(),
         ],
     ))
